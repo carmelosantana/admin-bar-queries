c55a6aac13e6e4237e14550a2806bc0248a272f5
Carmelo <me@carmelosantana.com>
2012-12-03 21:43:22 -0500

    Fixed compatibility with php 5.2.4. Changed timer action output priority to 999. Reduced space needed for timer output.

--- admin-bar-queries.php
+++ admin-bar-queries.php
@@ -3,22 +3,24 @@
 Plugin Name: Admin Bar Queries
 Plugin URI: http://carmelosantana.com/
 Description: Admin bar MySQL query and script execution timer output.
-Version: 0.5
+Version: 0.5.1
 Author: Carmelo Santana
 Author URI: http://carmelosantana.com
 */
 
-// Admin bar
-add_action( 'wp_before_admin_bar_render', function(){
+// add to admin bar
+add_action( 'wp_before_admin_bar_render', 'do_admin_bar_queries', 999 );
+
+// admin bar queries
+function do_admin_bar_queries(){
 	if ( !is_super_admin() )
 		return;
 	
 	global $wp_admin_bar;			
 	$wp_admin_bar->add_menu( array(
 		'parent' => false,
-		'id' => 'do_query_bar',
-		'title' => get_num_queries().'Q - '.timer_stop().'s', // link title
-		'href' => '#',
-		'meta' => false
-	));
-});
+		'id' => 'admin_bar_queries',
+		'title' => get_num_queries().'q '.timer_stop(0, 2).'s', // link title
+		'href' => '#'
+	));	
+}
