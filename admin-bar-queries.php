ead0c261c474b79ff938999116a8b581c17e01c2
Carmelo <me@carmelosantana.com>
2013-04-04 18:36:03 -0400

    Added sys_getloadavg() function check. Added load featuers to description. Version bump.

--- admin-bar-queries.php
+++ admin-bar-queries.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Admin Bar Queries
 Plugin URI: http://carmelosantana.com/
-Description: Admin bar MySQL query and script execution timer output.
+Description: MySQL queries, script timer and CPU load in your admin bar.
 Version: 0.5.2
 Author: Carmelo Santana
 Author URI: http://carmelosantana.com
@@ -15,22 +15,26 @@ add_action( 'wp_before_admin_bar_render', 'do_admin_bar_queries', 999 );
 function do_admin_bar_queries(){
 	if ( !is_super_admin() )
 		return;
-	
+
 	global $wp_admin_bar;			
 
+	// queries + timer
 	$wp_admin_bar->add_menu( array(
 		'id' => 'admin_bar_queries',
 		'title' => get_num_queries().'q '.timer_stop(0, 2).'s',
 		'href' => '#'
 	));
 
-	foreach ( $load = sys_getloadavg() as $c => $l )
-		$load[$c] = round($l, 2);
+	// if available, add load
+	if ( function_exists('sys_getloadavg') ){
+		foreach ( $load = sys_getloadavg() as $c => $l )
+			$load[$c] = round($l, 2);
 
-	$wp_admin_bar->add_menu( array(
-		'parent' => 'admin_bar_queries',
-		'id' => 'admin_bar_memory',
-		'title' => 'Load: <strong>' . $load[0] . /*', ' . $load[1] . ', ' . $load[2] .*/ '</strong>',
-		'href' => '#'
-	));
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'admin_bar_queries',
+			'id' => 'admin_bar_memory',
+			'title' => 'Load: <strong>' . $load[0] . /*', ' . $load[1] . ', ' . $load[2] .*/ '</strong>',
+			'href' => '#'
+		));
+	}
 }
